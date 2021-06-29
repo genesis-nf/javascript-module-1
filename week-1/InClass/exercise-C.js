c2386ef285c7c7f75c7bdd2ca451aacf210a1184
genesis-nf <79811065+genesis-nf@users.noreply.github.com>
2021-06-29 19:39:13 +0200

    Exercise-C texto

---
+++ week-1/InClass/exercise-C.js
@@ -0,0 +1,5 @@
+let variableTexto = "El tulipa es un género de plantas perennes y bulbosas perteneciente a la familia Liliaceae";
+
+console.log("variableTexto");
+console.log("variableTexto");
+console.log("variableTexto");
