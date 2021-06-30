1dccbffecb887debfee1881a9dc6245decff9a06
genesis-nf <79811065+genesis-nf@users.noreply.github.com>
2021-06-30 20:07:41 +0200

    typeof

--- week-1/InClass/exercise-D.js
+++ week-1/InClass/exercise-D.js
@@ -2,3 +2,8 @@
 let saludo ="Hola";
 let nombre ="Gardio";
 console.log(saludo + " " + nombre);
+
+const message = "This is a string";
+const messageType = typeof message;
+
+console.log(messageType); // logs 'string'
