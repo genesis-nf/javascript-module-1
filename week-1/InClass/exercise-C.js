8e753c0eba0453a9ac265f4050ecd84a58cae69d
genesis-nf <79811065+genesis-nf@users.noreply.github.com>
2021-06-30 19:58:43 +0200

    Variable greeting

--- week-1/InClass/exercise-C.js
+++ week-1/InClass/exercise-C.js
@@ -1,6 +1,6 @@
-let variableTexto = "El tulipa es un género de plantas perennes y bulbosas perteneciente a la familia Liliaceae";
+let greeting = "El tulipa es un género de plantas perennes y bulbosas perteneciente a la familia Liliaceae";
 
-console.log("variableTexto");
-console.log("variableTexto");
-console.log("variableTexto");
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
 
